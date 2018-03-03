be49cabdcf736b5084f16ef39e9dfdf5f67edf6a
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-03 15:23:00 -0300

    Atualiza e reorganiza os arquivos

---
+++ conteudo-igreja.php
@@ -0,0 +1,21 @@
+<div align="center">
+	<font color="#78373f" face="Verdana"><h3>Igreja Batista Represa Nova</h3></font>  
+</div>
+
+<div style="justify-content: flex-start">
+	<font color="black" face="arial">	 
+	A Igreja Batista Represa Nova é uma família que pertence a Deus, formado por homens e mulheres que creram em Cristo como seu Senhor e Salvador. Por este ato da graça de Deus as barreiras e inimizades entre as pessoas foram derrubadas e assim, repudiamos toda tentativa de manifestação preconceituosa de qualquer espécie entre nós.
+	<br><br> 
+	Assim nós existimos para: <b> Alcançar e treinar pessoas para discipular as nações.</b>
+	</font>
+	<div align="center">
+			<div style="padding-bottom: 6em"></div> 
+			<font color="black" face="arial">
+				"Não há salvação em nenhum outro, pois, debaixo do céu não há nenhum outro nome dado aos homens pelo qual devamos ser salvos" <b>(Atos 4.12)</b>.
+				<br><br>
+				"Pois Ele é a nossa paz, o qual de ambos fez um e destruiu a barreira, o muro da inimizade" <b>(Efésios 2.14)</b>.
+				<br><br>
+			</font>
+	</div>
+	<div style="padding-bottom: 1em"></div> 	
+</div>
