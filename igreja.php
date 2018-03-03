9fdc75491af4309ec2133659a371bf3973035b4b
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-03 15:26:21 -0300

    Remove o include da página não mais existente

--- igreja.php
+++ igreja.php
@@ -1,28 +1,16 @@
-﻿<?php $pagina = 'igreja'; ?>
+﻿<?php include("topo.php");?> 
+<div align="center" style="background-color:#78373f;border-radius: 25px">  
+  <div style="padding-bottom: 1em"></div>         
+  <img src="imagens/IGREJA/IMG_TELA_IGREJA.jpg" class="img-responsive" alt="...">
+  <div style="padding-bottom: 1em"></div>   
+  <?php include("menu-igreja.php");?>      
+</div>
 
-<?php include("topo.php");?>    
-
-       
-           
-          <div align="center" style="background-color:#78373f;border-radius: 25px">  
-            <div style="padding-bottom: 1em"></div>         
-            <img src="imagens/IGREJA/IMG_TELA_IGREJA.jpg" class="img-responsive" alt="...">
-            <div style="padding-bottom: 1em"></div>   
-            <?php include("menu-igreja.php");?>      
-          </div>  
-
-            <div style="background-color:#f2d0d4;border-radius: 10px">                 
-
-                  <object name="alvo" style="width: 100%;height: 400px" frameborder="0" data="conteudo-igreja.php" id="theframe"></object>               
-                  <script>
-          				var _theframe = document.getElementById("theframe");
-          				_theframe.contentWindow.location.href = _theframe.src;
-          				</script> 
-
-                 <?php include("imagem-igreja-direita.php");?>  
-          
-            </div>        
-         
-      
-     
-      <?php include("rodape.php");?>
+<div style="background-color:#f2d0d4;border-radius: 10px">
+  <object name="alvo" style="width: 100%;height: 400px" frameborder="0" data="conteudo-igreja.php" id="theframe"></object>               
+  <script>
+  var _theframe = document.getElementById("theframe");
+  _theframe.contentWindow.location.href = _theframe.src;
+  </script>
+</div> 
+<?php include("rodape.php");?>
