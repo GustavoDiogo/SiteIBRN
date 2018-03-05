64b58aef55cfa400f4e18a82a20f6324342d4087
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-05 00:44:39 -0300

    Reidenta o codigo e adiciona comentários

--- topo.php
+++ topo.php
@@ -1,78 +1,82 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Represa Nova</title>
+  <title>Represa Nova</title>
 
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/justified-nav.css" rel="stylesheet">   
+  <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/justified-nav.css" rel="stylesheet">   
 
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="http://cluster-piwik.locaweb.com.br/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 10006]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code --> 
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="http://cluster-piwik.locaweb.com.br/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 10006]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Piwik Code --> 
 
-<style type="text/css"> 
-  .header > li:hover {    
-    background-color:#87595b;
-    border-radius: 50px;
-  }
-</style>
+  <!-- Cor da seleção com o mouse em cima background-color -->
+  <style type="text/css"> 
+    .header > li:hover {    
+      background-color:#87595b;
+      border-radius: 50px;
+    }
+  </style>
 
-  </head>
-  <body>  
-      <nav class="navbar navbar-default navbar-fixed-top" style="border-bottom-right-radius:60px;border-bottom-left-radius:60px;background-color:#ffe2e4">  
-       <div class="container">      
-        <div class="navbar-header">
-          <button style="height:90px;background-color:#efefef;border-radius: 50px" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">            
-            <span style="background-color:#ff2d2d" class="icon-bar"></span>
-            <span style="background-color:#ff2d2d" class="icon-bar"></span>
-            <span style="background-color:#ff2d2d" class="icon-bar"></span>         
-          </button>
-          <a href="index.php">
-              <div style="width:200px">
-                  <img style="max-width: 100%; height: 80px; width: 80px;margin-left: 50px" src="imagens/logo_represanova.png"></span>
-              </div>
-          </a>
+</head>
+<body>  
+  <!-- Cor do fundo da barra de navegação fixa background-color -->
+  <nav class="navbar navbar-default navbar-fixed-top" style="border-bottom-right-radius:40px;border-bottom-left-radius:40px;background-color:#ffe2e4">  
+   <div class="container">      
+    <div class="navbar-header">
+      <!-- Cor do botão quando vai para versão mobile background-color -->
+      <button style="height:90px;background-color:#efefef;border-radius: 50px" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">            
+        <span style="background-color:#ff2d2d" class="icon-bar"></span>
+        <span style="background-color:#ff2d2d" class="icon-bar"></span>
+        <span style="background-color:#ff2d2d" class="icon-bar"></span>         
+      </button>
+      <a href="index.php">
+        <div style="width:200px">
+          <!-- Logo da Igreja, mudar o caminho em src para alterar -->
+          <img style="max-width: 100%; height: 80px; width: 80px;margin-left: 50px" src="imagens/logo_represanova.png"></span>
         </div>
-        <div class="container">
-          <div id="navbar" class="navbar-collapse collapse">    
-          <div style="padding-bottom: 1em"></div>     
-            <ul class="nav navbar-nav header" style="max-height: 100px"> 
-            <div style="padding-bottom: 0.5em"></div>                            
-              <li><a href="index.php">Home</a></li>
+      </a>
+    </div>
+    <div class="container">
+      <div id="navbar" class="navbar-collapse collapse">    
+        <div style="padding-bottom: 1em"></div>     
+        <ul class="nav navbar-nav header" style="max-height: 100px"> 
+          <div style="padding-bottom: 0.5em"></div>                            
+          <li><a href="index.php">Home</a></li>
 
-              <li><a href="igreja.php">A Igreja</a></li>
+          <li><a href="igreja.php">A Igreja</a></li>
 
-              <li><a href="ebd.php">Escola Bíblica</a></li>
+          <li><a href="ebd.php">Escola Bíblica</a></li>
 
-              <li><a href="devocionais.php">Devocionais</a></li>
+          <li><a href="devocionais.php">Devocionais</a></li>
 
-              <li><a href="cultos.php">Cultos</a></li>
+          <li><a href="cultos.php">Cultos</a></li>
 
-              <li><a href="ministerios.php">Ministérios</a></li>
+          <li><a href="ministerios.php">Ministérios</a></li>
 
-              <li><a href="contato.php">Contato</a></li>
+          <li><a href="contato.php">Contato</a></li>
 
-              <li><a href="represart.php">RepresArt</a></li>                     
-            </ul>
-          </div>
-        </div>        
-      </nav>
-    </body>
+          <li><a href="represart.php">RepresArt</a></li>                     
+        </ul>
+      </div>
+    </div>        
+  </nav>
+</body>
 
-    <div style="padding-bottom: 8.4em"></div> 
-     <div class="container">     
+<div style="padding-bottom: 8.4em"></div> 
+<div class="container">     
