c95cf09a11775e926446f28631b8691ac018a4f3
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-05 00:06:47 -0300

    Reidenta os códigos e volta a utilizar iframe devido ao navegador edge

--- igreja.php
+++ igreja.php
@@ -1,16 +1,21 @@
 ﻿<?php include("topo.php");?> 
+<!-- Cor do fundo da imagem da página background-color -->
 <div align="center" style="background-color:#78373f;border-radius: 25px">  
-  <div style="padding-bottom: 1em"></div>         
+  <div style="padding-bottom: 1em"></div>
+  <!-- Para alterar a imagem da página, basta mudar o caminho do src para outra imagem  -->
   <img src="imagens/IGREJA/IMG_TELA_IGREJA.jpg" class="img-responsive" alt="...">
   <div style="padding-bottom: 1em"></div>   
   <?php include("menu-igreja.php");?>      
 </div>
 
+<!-- Cor do fundo do texto após imagem e barra de navegação -->
 <div style="background-color:#f2d0d4;border-radius: 10px">
-  <object name="alvo" style="width: 100%;height: 512px" frameborder="0" data="conteudo-igreja.php" id="theframe"></object>               
-  <script>
+  <iframe name="alvo" style="width: 100%;height: 512px" frameborder="0" scrolling="yes" src="conteudo-igreja.php" id="theframe"></iframe>
+</div>
+
+<script>
   var _theframe = document.getElementById("theframe");
-  _theframe.contentWindow.data = _theframe.src;
-  </script>
-</div> 
+  _theframe.contentWindow.location.href = _theframe.src;
+</script> 
+
 <?php include("rodape.php");?>
--- menu-igreja.php
+++ menu-igreja.php
@@ -3,27 +3,29 @@
 		flex-direction: row;
 		flex-wrap: wrap;
 		text-align:center;
-	}	
+	}
+	/* Cor da seleção com o mouse em cima background-color*/
 	.navbar-igreja > ul > li:hover {
 		background-color:#87595b;
-    	border-radius: 50px;
+		border-radius: 50px;
 	}	
 </style>
 
 <script type="text/javascript">
-	function scrollDown(){
-		$('html,body').animate({ scrollTop: 400 }, 'slow');
+	function scrollDown(pixels){		
+		$('html,body').animate({ scrollTop: pixels }, 'slow');
 	}
 </script>
 
-	<div class="navbar navbar-default" style="background-color: #78373f;border: none ;border-radius: 25px">
-		<div class="navbar-collapse navbar-igreja">		  
-			<ul class="nav navbar-nav">				
-				<li><a href="conteudo-igreja.php" target="alvo" style="color: white" onClick="scrollDown()"> A Igreja</a></li>
-				<li><a href="conteudo-igreja-pastor.php" target="alvo" style="color: white"> Pastor</a></li>
-				<li><a href="conteudo-igreja-escrituras.php" target="alvo" style="color: white"> Exame das Escrituras</a></li>
-				<li><a href="conteudo-igreja-compromisso-social.php" target="alvo" style="color: white"> Compromisso Social</a></li>
-				<li><a href="conteudo-igreja-comente.php" target="alvo" style="color: white"> Comente</a></li>				
-			</ul>			
-		</div>
-	</div>	
+<!-- Cor da barra de navegação background-color -->
+<div class="navbar navbar-default" style="background-color: #78373f;border: none ;border-radius: 25px">
+	<div class="navbar-collapse navbar-igreja">		  
+		<ul class="nav navbar-nav">				
+			<li><a href="conteudo-igreja.php" target="alvo" style="color: white" onClick="scrollDown(400)"> A Igreja</a></li>
+			<li><a href="conteudo-igreja-pastor.php" target="alvo" style="color: white"> Pastor</a></li>
+			<li><a href="conteudo-igreja-escrituras.php" target="alvo" style="color: white"> Exame das Escrituras</a></li>
+			<li><a href="conteudo-igreja-compromisso-social.php" target="alvo" style="color: white"> Compromisso Social</a></li>
+			<li><a href="conteudo-igreja-comente.php" target="alvo" style="color: white"> Comente</a></li>				
+		</ul>			
+	</div>
+</div>	
