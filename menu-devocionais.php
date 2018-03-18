7551309df96998488ca22229679af24b84c3f45a
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-17 22:09:23 -0300

    Quick fix

--- menu-devocionais.php
+++ menu-devocionais.php
@@ -21,10 +21,10 @@
 <div class="navbar navbar-default" style="background-color:#632731;border: none ;border-radius: 25px">
 	<div class="navbar-collapse navbar-devocionais">		  
 		<ul class="nav navbar-nav">			
-			<li><a href="devocional-atual.php" target="alvo" style="color: white" onClick="scrollDown(340)"> Devocional Atual</a></li>				
-			<li><a href="conteudo-devocionais-anteriores.php" target="alvo" style="color: white" onClick="scrollDown(340)"> Anteriores</a></li>
-			<li><a href="conteudo-devocionais-whatsapp.php" target="alvo" style="color: white" onClick="scrollDown(340)"> WhatsApp</a></li>
-			<li><a href="conteudo-devocionais-comente.php" target="alvo" style="color: white" onClick="scrollDown(340)"> Comente</a></li>				
+			<li><a href="devocional-atual.php" target="alvo" style="color: white" onClick="scrollDown(345)"> Devocional Atual</a></li>				
+			<li><a href="conteudo-devocionais-anteriores.php" target="alvo" style="color: white" onClick="scrollDown(345)"> Anteriores</a></li>
+			<li><a href="conteudo-devocionais-whatsapp.php" target="alvo" style="color: white" onClick="scrollDown(345)"> WhatsApp</a></li>
+			<li><a href="conteudo-devocionais-comente.php" target="alvo" style="color: white" onClick="scrollDown(345)"> Comente</a></li>				
 		</ul>			
 	</div>
 </div>
