fbe3ea6260b479b20fc98039dc2f156ffe5b90cf
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-17 22:08:36 -0300

    Reestrutra as páginas do culto

--- menu-cultos.php
+++ menu-cultos.php
@@ -1,13 +1,28 @@
+<style type="text/css">
+	.navbar-cultos{
+		flex-direction: row;
+		flex-wrap: wrap;
+		text-align:center;
+	}
+	/* Cor da seleção com o mouse em cima background-color*/
+	.navbar-cultos > ul > li:hover {
+		background-color:#87595b;
+		border-radius: 50px;
+	}	
+</style>
 
-                
-                
-                <ul class="list-unstyled" style="margin-bottom:30px;">
-                <li><img src="imagens/DEVOCIONAIS/icone_lat_devocionais.gif"><a href="conteudo-cultos-ao-vivo.php" target="alvo"> CULTOS
-                </a> </li>
+<script type="text/javascript">
+	function scrollDown(pixels){		
+		$('html,body').animate({ scrollTop: pixels }, 'slow');
+	}
+</script>
 
-                <li><img src="imagens/DEVOCIONAIS/icone_lat_devocionais.gif"><a href="conteudo-cultos-comente.php" target="alvo"> COMENTE</a></li>
-                </ul>
-                
-                 <img src="imagens/CULTOS/CULTOS_img_lateral_esq.jpg" class="img-thumbnail" style="background-color: #632731; vertical-align: bottom;display: table-cell;" alt="...">
-
-                
+<!-- Cor da barra de navegação background-color -->
+<div class="navbar navbar-default" style="background-color:#632731;border: none ;border-radius: 25px">
+	<div class="navbar-collapse navbar-cultos">		  
+		<ul class="nav navbar-nav">
+			<li><a href="conteudo-cultos-ao-vivo.php" target="alvo" style="color: white" onClick="scrollDown(265)"> Culto ao vivo</a></li>
+			<li><a href="conteudo-cultos-comente.php" target="alvo" style="color: white" onClick="scrollDown(265)"> Comente</a></li>				
+		</ul>			
+	</div>
+</div>        
