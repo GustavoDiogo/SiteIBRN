ff828f26a0c99a1fc2671acb193b0a17f37095da
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-03-24 14:43:52 -0300

    Corrige o nome do arquivo

---
+++ conteudo-ministerios.php
@@ -0,0 +1,21 @@
+<div align="center">
+	<!-- Cor do título -->
+	<font color="#9e7535" face="Verdana"><h3>União de Ministérios</h3></font>  
+</div>
+
+<div style="justify-content: flex-start;text-align: justify;text-justify: inter-word">
+	<!-- Conteúdo -->
+	<font color="black" face="arial">
+		<div align="center">
+			<p><i>"Cada um exerça o dom que recebeu para servir os outros, administrando fielmente a graça de Deus em suas múltiplas formas"</i> <b>(I Pedro 4.10)</b></p>
+			<p><i>"Assim, quer vocês comam, bebam ou façam qualquer outra coisa, façam tudo para a glória de Deus"</i> <b>(I Coríntios 10.31)</b></p>		
+		</div>
+		<p style="text-indent: 20px">
+			Na Igreja Batista Represa Nova acreditamos que servir a Deus é para a vida toda, da mesma forma que adorar a Deus a vida toda. E servimos a Deus servindo pessoas, visando a glória de Deus. Na IBRN você é incentivado e ajudado a servir e fazer aquilo para o que Deus lhe capacitou fazer. Assim nós valorizamos:
+			<div align="center">
+				<b>Princípios bíblicos na sociedade contemporânea para: família, relacionamentos, ética, negócios e em tudo, visando sempre a glória de Deus.</b>
+			</div>
+		</p>
+	</font>
+	<div style="padding-bottom: 1em"></div> 	
+</div>
