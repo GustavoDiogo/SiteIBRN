dea253b36b1763a77e97f7f200118597d36a69d2
root <root@debian>
2018-03-25 17:28:47 -0300

    Reformula as páginas, tirando a do espetaculos e oficina

---
+++ conteudo-historico.php
@@ -0,0 +1,27 @@
+<div align="center">
+	<!-- Cor do título -->
+	<font color="black" face="Verdana"><h3>Histórico</h3></font>  
+</div>
+
+<div style="justify-content: flex-start;text-align: justify;text-justify: inter-word">
+	<!-- Conteúdo -->
+	<font color="black" face="arial">
+		<p style="text-indent: 20px">
+			O nome Represart foi criado por um grupo anterior ao nosso,  na Igreja Batista em Represa Nova. Os anos se passaram e o Represart findou.
+		</p>
+		<p style="text-indent: 20px">
+			Quando Leonardo Dias e Lilian Gruenwadt idealizaram o projeto de peças autorais e releituras, com intuito evangelistico, assim como o grupo anterior e, ainda, em Represa Nova, os membros que fizeram parte do antigo projeto, nos apresentaram o Represart, então não vimos nome mais conveniente e chamoso, além de ser uma homenagem a todos que já participaram e queriam retornar.
+		</p>
+		<p style="text-indent: 20px">
+			Em Novembro de 2012, demos início as produções de novas montagens teatrais, cantatas encenadas e musicais.
+		</p>
+		<p style="text-indent: 20px">
+			Continuaremos em cena, até quando Deus, nosso pai, assim permitir.
+		</p>
+		<div align="center">
+			<div style="padding-bottom: 3em"></div>
+			<img src="imagens/REPRESART/foto_historico.jpg" style="max-width: 100%">
+		</div>
+	</font>
+	<div style="padding-bottom: 1em"></div> 	
+</div>
