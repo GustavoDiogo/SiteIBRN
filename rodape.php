eff0bc329eeb62753c8bf82e230711138c2cebfe
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-04-01 17:41:41 -0300

    Atualiza o rodapé

--- rodape.php
+++ rodape.php
@@ -1,54 +1,24 @@
-      <!-- Site footer -->
-      <footer class="footer">
-      <style type="rodapefont" > </style>      
-      <div align="right">
-       <div style="color:#e2deea"> 
-       <font size=1>Imagens e ilustrações por freepik.com</font>
-       </div>
-       </div>
-       <div class="row col-center">
-        <div class="col-lg-3 txt-center display-table">
-        <img src="imagens/logo_represanova.png">
-        </div><!--
-    -->
-        
-        <div class="col-lg-5 display-cell" style="height:136px;">
-        <p>ALCANÇAR E TREINAR PESSOAS PARA DISCIPULAR AS NAÇÕES</p>
-        </div>
-        
-        <div class="col-lg-1 text-right display-cell">
-        <img src="imagens/barra_rodape.png">
-        </div>
-        
-        <div class="col-lg-3  display-cell">
+<footer class="footer">
+  <div style="float: left">
+    <img src="imagens/logo_represanova.png">
+  </div>
+  <div align="right">
+    <h4>
+      Rua Silvia de Barros, 125 - Pedreira<br>
+      CEP 04462-010, São Paulo - SP<br>
+      (11) 5611-6811
+    </h4>      
+    <div style="padding-bottom: 1em"></div>      
+    <a href="https://www.youtube.com/channel/UCvNtPmmcjh_2N4hZYZruawA" target="_blank"><img src="imagens/icon_youtube.png"></a>
+    <a href="https://www.facebook.com/Igreja-Batista-Represa-Nova-204589722917650/?ref=ts&fref=ts" target="_blank"><img src="imagens/icon_fb.png"></a> 
+    <a href="https://www.instagram.com/ibrepresanova/" target="_blank"><img src="imagens/icon_insta.png"></a>
+    <br>      
+    <font size=0.5>Imagens e ilustrações por freepik.com</font>       
+  </div>
+</footer>
 
-
-        <small>RUA SILVIA DE BARROS, 125 - PEDREIRA
-        CEP 04462-010 - SÃO PAULO -SP<br>
-        TEL:(11)5611-6811</small>
-
-<p>
-        <a href="https://www.youtube.com/channel/UCvNtPmmcjh_2N4hZYZruawA" target="_blank">
-        <img src="imagens/icon_youtube.png"> </a>
-        <a href="https://www.facebook.com/Igreja-Batista-Represa-Nova-204589722917650/?ref=ts&fref=ts" target="_blank"> 
-        <img src="imagens/icon_fb.png"></a> 
-        <a href="https://www.instagram.com/ibrepresa_nova/" target="_blank">
-        <img src="imagens/icon_insta.png"></p></a>
-
-
-        
-        </div>
-       </div>
-      </footer>
-
-    </div> <!-- /container -->
-
-
-	    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  </body>
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
 </html>
